e4332a88deb42647cc9bbc7d53ed262b9640021d
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-02-24 11:15:38 +0900

    update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,8 @@ cmake $RECIPE_DIR/scripts
 cd ..
 
 vcpkg install
-cmake . -B build -DMINIO_CPP_TEST=ON -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-cmake --build ./build --config Debug
+cmake . -B build -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build
 
 make -j$(nproc)
 make install
